faf3f100e82352333faca7a1f12e246eea87b9b8
JoKyungseo <eksql0645@gmail.com>
2022-10-26 17:14:10 +0900

    [Fix] weater API axios 적용

--- utils/weather.js
+++ utils/weather.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
-const requestPromise = require('request-promise');
+const axios = require('axios');
 
 async function weather(countryCode) {
   try {
-    const q = countryCode || 'Seoul';
-    const options = {
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${process.env.API_KEY}&lang=kr`,
-      method: 'GET',
+    const data = countryCode || 'Seoul';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${process.env.API_KEY}&lang=kr`;
+
+    const config = {
       headers: { 'content-type': 'application/json' },
     };
 
-    let res = await requestPromise(options);
-    res = JSON.parse(res);
-    const weather = res.weather[0].description;
-    return weather;
+    const res = await axios.get(url, config);
+
+    return res.data.weather[0].description;
   } catch (err) {
-    return null;
+    throw new Error(err);
   }
 }
 
